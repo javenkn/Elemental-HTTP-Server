179ef9de560fd75096494736a980e4f96d44c970
javenkn <javenkn7@yahoo.com>
2016-07-06 02:36:34 -1000

    got the GET method to work with the server

---
+++ server.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var fs = require('fs');
+var CONFIG = require('./config');
+var PUBLIC = './public';
+
+var server = http.createServer((request, response) => {
+  console.log('Somebody connected.');
+  var filepath = PUBLIC + request.url; // file path
+
+  fs.readFile(filepath, 'utf8', (error, data) => {
+    if(error){ // if there is an error (file doesn't exist)
+      response.statusCode = 404;
+      response.statusMessage = 'Not Found';
+      filepath = PUBLIC + '/404.html';
+      fs.readFile(filepath, 'utf8', (error, data) => {
+        response._contentLength = data.length;
+        response.write(data);
+        response.end('Done sending.');
+      });
+    }else{ // if the file exists
+      response.statusCode = 200;
+      response.statusMessage = 'OK';
+      response._contentLength = data.length;
+      response.write(data);
+      response.end('Done sending.');
+    }
+  });
+});
+
+
+server.listen(CONFIG.PORT, () => {
+  var PORT = server.address().port;
+  console.log('Listening on port', PORT);
+});
