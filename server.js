a0d2cd8930f0b2036e7f342448167ba5e2b87f50
javenkn <javenkn7@yahoo.com>
2016-07-06 21:57:52 -1000

    added functions to make my code cleaner. Everything works with POST, GET, and HEAD methods. Tested it with Postman.

--- server.js
+++ server.js
@@ -10,85 +10,48 @@ var server = http.createServer((request, response) => {
   // creates an array to put all of the form values in
   var postValues = [];
   var dataBuff = '';
+
   request.on('data', (data) => {
     dataBuff += data;
   });
 
   request.on('end', () => {
-    // gets the request and organizes it into an object
-    var requestElementObj = querystring.parse(dataBuff.toString());
-    // gets the keys from the object
-    var requestKeys = Object.keys(requestElementObj);
+    getNeededValues(dataBuff, postValues);
 
-    // go through the request keys and push each key into the array
-    requestKeys.forEach(function (element) {
-      postValues.push(requestElementObj[element]);
-    });
-
-    var HTMLContent = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>The Elements - ` + postValues[0] +
-  `</title>
-  <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-  <h1>` + postValues[0] + `</h1>
-  <h2>` + postValues[1] + `</h2>
-  <h3>Atomic number ` + postValues[2] + `</h3>
-  <p>` + postValues[3] + `</p>
-  <p><a href="/">back</a></p>
-</body>
-</html>`;
+    var HTMLContent = createHTMLContent(postValues);
 
     var filepath = PUBLIC + request.url; // file path
     // checks if there is no request url
-    if(filepath === './public/'){
+    if(filepath === './public/') {
       filepath = PUBLIC + '/index.html';
     }
 
     // for /elements add a .html to it so that we can check if
-    if(filepath.slice(-5) !== '.html'){
+    if(filepath.slice(-5) !== '.html') {
       filepath = filepath + '.html';
     }
     // console.log(HTMLContent);
 
     fs.readFile(filepath, 'utf8', (error, data) => {
       if(error){ // if there is an error (file doesn't exist)
-
-        if(request.method === 'POST') {
-          fs.writeFile(filepath, HTMLContent,'utf8', (error) => {
-            console.log('Finished writing.');
-          });
-        }
-
         response.statusCode = 404;
         response.statusMessage = 'Not Found';
-        filepath = PUBLIC + '/404.html';
-        fs.readFile(filepath, 'utf8', (error, data) => {
-          if(request.method === 'GET') {
-            response.setHeader('Content-Length', data.length);
-            response.write(data);
-          }else if(request.method === 'HEAD') {
-            response.setHeader('Content-Length', data.length);
-          }
-          response.end();
-        });
+        if(request.method === 'POST') {
+          sendPostResponse(response, filepath, HTMLContent);
+        } else {
+          filepath = PUBLIC + '/404.html';
+          fs.readFile(filepath, 'utf8', (error, data) => {
+            sendResponse(request, response, filepath, HTMLContent, data);
+          });
+        }
       }else{ // if the file exists
         response.statusCode = 200;
         response.statusMessage = 'OK';
-        if(request.method === 'GET') {
-          response.setHeader('Content-Length', data.length);
-          response.write(data);
-        } else if(request.method === 'HEAD') {
-          response.setHeader('Content-Length', data.length);
-        } else if(request.method === 'POST') {
-          fs.writeFile(filepath, HTMLContent,'utf8', (error) => {
-            console.log('Finished writing.');
-          });
+        if(request.method === 'POST') {
+          sendPostResponse(response, filepath, HTMLContent);
+        } else {
+          sendResponse(request, response, filepath, HTMLContent, data);
         }
-        response.end();
       }
     }); // fs read
   }); // end event code
@@ -100,3 +63,54 @@ server.listen(CONFIG.PORT, () => {
   var PORT = server.address().port;
   console.log('Listening on port', PORT);
 });
+
+function createHTMLContent(elementValueArray) {
+  var content = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>The Elements - ` + elementValueArray[0] +
+  `</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <h1>` + elementValueArray[0] + `</h1>
+  <h2>` + elementValueArray[1] + `</h2>
+  <h3>Atomic number ` + elementValueArray[2] + `</h3>
+  <p>` + elementValueArray[3] + `</p>
+  <p><a href="/">back</a></p>
+</body>
+</html>`;
+
+  return content;
+}
+
+function getNeededValues (data, array) {
+  // gets the request and organizes it into an object
+  var requestElementObj = querystring.parse(data.toString());
+  // gets the keys from the object
+  var requestKeys = Object.keys(requestElementObj);
+
+  // go through the request keys and push each key into the array
+  requestKeys.forEach(function (element) {
+    array.push(requestElementObj[element]);
+  });
+}
+
+function sendPostResponse (response, filepath, HTMLContent) {
+  fs.writeFile(filepath, HTMLContent,'utf8', (error) => {
+    console.log('Finished writing.');
+  });
+  response.end();
+}
+
+function sendResponse (request, response, filepath, HTMLContent, data) {
+  if (request.method === 'GET') {
+    response.setHeader('Content-Length', data.length);
+    response.write(data);
+    response.end();
+  } else if(request.method === 'HEAD') {
+    response.setHeader('Content-Length', data.length);
+    response.end();
+  }
+}
